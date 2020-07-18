2744356f467ae243d763cd6fc4fc39f213b077a7
Shawn <shdsouza@seek.com.au>
2020-07-18 18:33:48 +1000

    update index

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home({ allPostsData }) {
         <h1 className="title">
           Read{' '}
           <Link href="/posts/first-post">
-            <a>this page!</a>
+            <a>this page2!</a>
           </Link>
         </h1>
 
