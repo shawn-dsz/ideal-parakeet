c6eeb4bb00391d9125443086cc9c1ffa25b6dd28
Shawn <shdsouza@seek.com.au>
2020-07-18 17:16:31 +1000

    list posts

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,39 @@
 import Head from 'next/head';
 import Link from 'next/link';
-
-export default function Home() {
+import utilStyles from '../styles/utils.module.css';
+import Layout from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData() || [];
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+export default function Home({ allPostsData }) {
   return (
     <div className="container">
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Layout home>
+        <section className={utilStyles.headingMd}>…</section>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+          </ul>
+        </section>
+      </Layout>
       <main>
         <h1 className="title">
           Read{' '}
