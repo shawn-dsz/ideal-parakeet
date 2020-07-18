c10fd2955227bdf8171bb1a0b2bc18919758e077
Shawn <shdsouza@seek.com.au>
2020-07-18 17:29:25 +1000

    add api

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import utilStyles from '../styles/utils.module.css';
 import Layout from '../components/layout';
 import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/date';
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData() || [];
   return {
@@ -24,11 +25,13 @@ export default function Home({ allPostsData }) {
           <ul className={utilStyles.list}>
             {allPostsData.map(({ id, date, title }) => (
               <li className={utilStyles.listItem} key={id}>
-                {title}
+                <Link href="/posts/[id]" as={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
                 <br />
-                {id}
-                <br />
-                {date}
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
               </li>
             ))}
           </ul>
